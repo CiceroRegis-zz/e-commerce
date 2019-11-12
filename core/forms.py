21ee24fd49d9e296be51661dd66c53f1a2d440c9
CiceroRegis <ciceroregis25@gmail.com>
2019-11-12 12:00:42 -0300

    finalizing contact form

---
+++ core/forms.py
@@ -0,0 +1,77 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class ContactForm(forms.Form):
+    nome_completo = forms.CharField(
+        error_messages={'required': 'Obrigatório o preenchimento do nome'},
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Seu nome completo'
+            }
+        )
+    )
+    email = forms.EmailField(
+        error_messages={'invalid': 'Digite um email válido'},
+        widget=forms.EmailInput(
+
+            attrs={
+                "class": "form-control",
+                "placeholder": " Your email"
+            }
+        )
+    )
+    mensagem = forms.CharField(
+        error_messages={'required': 'È obrigatorio o preenchimento do campo mensagem!'},
+        widget=forms.Textarea(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite sua mensagem'
+            }
+        )
+    )
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if not 'gmail' in email:
+            raise forms.ValidationError('O email deve ser do gmail.com')
+        return email
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(
+        widget=forms.PasswordInput
+    )
+
+
+class RegisterForm(forms.Form):
+    username = forms.CharField()
+    email = forms.EmailField()
+    password = forms.CharField(
+        widget=forms.PasswordInput
+    )
+    confirm_password = forms.CharField(label='Confirme sua seha',
+                                       widget=forms.PasswordInput)
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        queryset = User.objects.filter(username=username)
+        if queryset.exists():
+            raise forms.ValidationError('Esse usuario já existe, escolha outro nome')
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        queryset = User.objects.filter(email=email)
+        if queryset.exists():
+            raise forms.ValidationError('Esse email já existe, tente outro')
+        return email
+
+    def clean_confirm_password(self):
+        data = self.cleaned_data
+        password = self.cleaned_data['password']
+        confirm_password = self.cleaned_data['confirm_password']
+        if password != confirm_password:
+            raise forms.ValidationError('As senhas informadas deve ser iguais')
+        return data
--- core/views.py
+++ core/views.py
@@ -1,5 +1,49 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from core.forms import ContactForm
+from django.contrib.auth import authenticate, login, get_user_model
+from .forms import ContactForm, LoginForm, RegisterForm
+
+
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        'form': form
+    }
+
+    print('user logged in')
+    if form.is_valid():
+        print(form.cleaned_data)
+    username = form.cleaned_data['username']
+    password = form.cleaned_data['password']
+    user = authenticate(request, username=username, password=password)
+    print(user)
+    if user is not None:
+        login(request, user)
+        print("Login valido")
+        return redirect('/')
+    else:
+        print('Login invalido!')
+        return render(request, 'auth/login.html', context)
+
+
+User = get_user_model()
+
+
+def register_page(request):
+    form = RegisterForm(request.POST or None)
+    context = {
+        'form': form
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data['username']
+        email = form.cleaned_data['email']
+        password = form.cleaned_data['password']
+        new_user = User.objects.create_user(username, email, password)
+        print(new_user)
+        return render(request, 'auth/register.html', context)
 
 
 def home_page(request):
@@ -7,6 +51,8 @@ def home_page(request):
         'title': 'pagina principal',
         'content': 'Bem vindo a pagina principal'
     }
+    if request.user.is_authenticated:
+        context['premium_content'] = 'Voçe é um usuario Premium'
     return render(request, "home_page.html", context)
 
 
@@ -19,8 +65,15 @@ def about_page(request):
 
 
 def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
     context = {
         'title': 'pagina de contato',
-        'content': 'Bem-vindo a pagina de contato'
+        'content': 'Bem-vindo a pagina de contato',
+        'form': contact_form
     }
+    if request.method == 'POST' and contact_form.is_valid():
+        contact_form.cleaned_data
+        print(contact_form.cleaned_data)
+    # if request.method == 'POST':
+    #     print(request.POST)
     return render(request, 'contact/view.html', context)
