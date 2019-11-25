d5c05ce0556d4c84ac897f97033be06107454c75
CiceroRegis <ciceroregis25@gmail.com>
2019-11-25 12:33:57 -0300

    update files

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -82,16 +82,16 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'ecommerce',
-#         'USER': 'root',
-#         'PASSWORD': 'Meups32019',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#     }
-# }
+ DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'ecommerce',
+         'USER': 'root',
+         'PASSWORD': 'Meups32019',
+         'HOST': 'localhost',
+         'PORT': '3306',
+     }
+ }
 
 DATABASES = {
     'default': {
