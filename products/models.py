1aa7a4ffa6c04d2526398600c496f1dd85f3d1f5
CiceroRegis <ciceroregis25@gmail.com>
2019-11-29 22:11:25 -0300

    migrate

--- products/models.py
+++ products/models.py
@@ -41,7 +41,8 @@ class Product(models.Model):  # product model
 
     title = models.CharField(max_length=120)
     description = models.TextField()
-    price = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
+    price = models.DecimalField(
+        decimal_places=2, max_digits=20, default=100.00)
     image = FileBrowseField('products', max_length=200, null=True, blank=False)
     slug = models.SlugField(blank=True, unique=True)
     featured = models.BooleanField(default=False)
